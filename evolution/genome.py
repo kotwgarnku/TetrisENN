e3fefb3b99d6d1815208862fcbef632900d183e6
Kacper Kamiński <lampo100@gmail.com>
2017-12-26 21:31:22 +0100

    ConnectionGene is done; add Gene creation from list of Connections; add few tests

--- evolution/genome.py
+++ evolution/genome.py
@@ -2,8 +2,40 @@
 class Genome:
     _genome_ID = 0
 
-    def __init__(self):
-        pass
+    def __init__(self, connections):
+        self.node_genes = {}
+        self.connection_genes = {}
+
+        for connection in connections:
+            if len(connection) != 4:
+                raise Exception('Connaction does not contain all necessary informations')
+
+            source_node = self._find_node(connection[0])
+            dest_node = self._find_node(connection[1])
+            if source_node is None:
+                source_node = self._create_new_node(connection[0])
+            if dest_node is None:
+                dest_node = self._create_new_node(connection[1])
+
+            weight = connection[2]
+            enabled = connection[3]
+
+            if (source_node.node_id, dest_node.node_id) in self.connection_genes:
+                raise Exception('Connections must be unique')
+            else:
+                self.connection_genes[(source_node.node_id, dest_node.node_id)] = ConnectionGene(source_node, dest_node,
+                                                                                                 weight, enabled)
+
+    def _find_node(self, node_gene_id):
+        if node_gene_id in self.node_genes:
+            return self.node_genes[node_gene_id]
+        else:
+            return None
+
+    def _create_new_node(self, node_id):
+        self.node_genes[node_id] = NodeGene(node_id=node_id)
+        return self.node_genes[node_id]
+
 
     def mutate(self):
         pass
@@ -16,21 +48,30 @@ class Genome:
 
 
 class NodeGene:
-    def __init__(self):
-        self.node_number = -1
-        self.node_type = None
+    def __init__(self, node_id=-1, node_type='hidden'):
+        self.node_id = node_id
+        self.node_type = node_type
 
 
 class ConnectionGene:
     _innovation_number = 0
 
-    def _get_innovation_number(self):
+    def __init__(self, source_node=None, destination_node=None, weight=1, enabled=False):
+        self.source_node = source_node
+        self.destination_node = destination_node
+        self._check_connection_vialability()
+        self.weight = weight
+        self.enabled = enabled
+        self.innovation_number = self._get_innovation_number()
+
+    def _check_connection_vialability(self):
+        if self.source_node is None or self.destination_node is None:
+            raise Exception('One node is empty')
+
+    def get_connection(self):
+        return self.source_node.node_id, self.destination_node.node_id, self.weight, self.enabled
+
+    @staticmethod
+    def _get_innovation_number():
         ConnectionGene._innovation_number += 1
         return ConnectionGene._innovation_number - 1
-
-    def __init__(self, input_node = None, output_node = None, weight = 1, enabled = False):
-        self.input_node = input_node
-        self.output_node = output_node
-        self.weight = None
-        self.enabled = False
-        self.innovation_number = self._get_innovation_number()
--- tests/genometest.py
+++ tests/genometest.py
@@ -1,23 +1,39 @@
 import unittest
 
-from evolution.genome import ConnectionGene
+from evolution.genome import ConnectionGene, NodeGene, Genome
+
+
+class TestConnectionGeneCase(unittest.TestCase):
+    def setUp(self):
+        self.connectionGene1 = ConnectionGene(NodeGene(), NodeGene())
+        self.connectionGene2 = ConnectionGene(NodeGene(), NodeGene())
+        self.connectionGene3 = ConnectionGene(NodeGene(), NodeGene())
+
+    def test_innovation_number_assignment1(self):
+        self.assertEqual(self.connectionGene1.innovation_number, 0)
+        self.assertEqual(self.connectionGene2.innovation_number, 1)
+        self.assertEqual(self.connectionGene3.innovation_number, 2)
 
 
 class TestGenomeCase(unittest.TestCase):
 
-    def test_innovation_number_assignment1(self):
-        genome1 = ConnectionGene()
-        genome2 = ConnectionGene()
-        genome3 = ConnectionGene()
-        self.assertEqual(genome1.innovation_number, 0)
-        self.assertEqual(genome2.innovation_number, 1)
-        self.assertEqual(genome3.innovation_number, 2)
+    def test_invalid_connection_exception(self):
+        with self.assertRaises(Exception):
+            ConnectionGene(NodeGene())
+            ConnectionGene()
 
-    def test_innovation_number_assignment2(self):
-        genome = ConnectionGene()
-        self.assertEqual(genome.innovation_number, 3)
+    def test_uniqueness_of_connections(self):
+        connections = [[1, 3, 0, True], [1, 3, 0, True]]
+        with self.assertRaises(Exception):
+            Genome(connections)
+
+    def test_genome_creation(self):
+        connections = [[1, 3, 0, True], [1, 4, 0, True], [2, 3, 0, True], [2, 4, 0, True]]
+        genome = Genome(connections)
 
 
 if __name__ == '__main__':
-    suite = unittest.TestLoader().loadTestsFromTestCase(TestGenomeCase)
-    unittest.TextTestRunner(verbosity=2).run(suite)
+    firstSuite = unittest.TestLoader().loadTestsFromTestCase(TestGenomeCase)
+    secondSuite = unittest.TestLoader().loadTestsFromTestCase(TestConnectionGeneCase)
+    unittest.TextTestRunner(verbosity=2).run(firstSuite)
+    unittest.TextTestRunner(verbosity=2).run(secondSuite)
