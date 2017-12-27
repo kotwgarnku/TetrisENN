6a5b3548f507880cae336f997db93c9ecb6d413d
Kacper Kamiński <lampo100@gmail.com>
2017-12-27 01:23:34 +0100

    Method generating neural network from genome is done

--- nn/neuralnetwork.py
+++ nn/neuralnetwork.py
@@ -1,19 +1,39 @@
 import numpy as np
 
+
 class NeuralNetwork:
     def __init__(self):
         self._connections = {}
+        self._neurons = {}
+        self._input_size = 0
+        self._output_size = 0
+        self._input_neurons = {}
+        self._output_neurons = {}
 
-    def generate_network(self, genome):
+    def generate_network(self, genome, input_size, output_size):
+        self._input_size = input_size
+        self._output_size = output_size
         self.connectionss = sorted(genome.get_connections())
         connections = sorted(genome.get_connections())
         for source, destination, weight, enabled in connections:
-            self._connections[(source, destination)] = Neuron()
+            if source not in self._neurons:
+                self._neurons[source] = Neuron()
+            if destination not in self._neurons:
+                self._neurons[destination] = Neuron()
+            if enabled:
+                self._connections[(source, destination)] = weight
 
+        for index in range(1, input_size + 1):
+            self._input_neurons[index] = self._neurons[index]
 
+        for index in range(input_size + 1, len(self._neurons)):
+            self._output_neurons[index] = self._neurons[index]
 
-    def forward(self):
-        pass
+    def forward(self, input=[]):
+        if(len(input) != self._input_size):
+            raise Exception("Expected {!s} inputs, got {!s} instead".format(self._input_size, len(input)))
+        connections = sorted(self._connections)
+
 
 
 class Neuron:
