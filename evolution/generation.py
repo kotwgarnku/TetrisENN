da748e248731ccde54601f6a9b575f57e2060eb4
Kacper Kamiński <lampo100@gmail.com>
2018-01-04 23:54:57 +0100

    Add docstrings

--- evolution/generation.py
+++ evolution/generation.py
@@ -1,29 +1,36 @@
 class Generation():
     def __init__(self):
-        self.spiecies = {}
+        self.species = {}
 
     def create_new_generation(self):
+        '''
+        Creates and returns new Generation of species based on current generation.
+        '''
         pass
 
     def adjust_fitnesses(self):
-        pass
-
-    def reproduce(self):
+        '''
+        Adjust fitness scores of every genome in every species.
+        '''
         pass
 
     def compute_children_number(self):
-        pass
-
-    def mate(self):
+        '''
+        Computes and returns list of children that every species should have.
+        :return:
+        '''
         pass
 
 
 class Species():
-    def adjust_genomes_fitnesses(self):
+    def adjust_genomes_fitnesses(self, generationFitnessScore):
+        '''
+        Adjusts fitness score of every genome in the species.
+        '''
         pass
 
     def get_parents(self):
-        pass
-
-    def set_spiecies(self):
+        '''
+        Returns parents that will reproduce.
+        '''
         pass
