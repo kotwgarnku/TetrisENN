189adee85b23f8af399c4c949fbb3fbd9ae5fe8e
Tomasz Galecki <saleph@protonmail.com>
2018-01-04 13:23:39 -0800

    add compatibility distance utilities with tests

--- evolution/util.py
+++ evolution/util.py
@@ -1,2 +1,64 @@
 def sort_connections_by_innovation_number(connections):
     return sorted(connections.values(), key=lambda connection: connection.innovation_number)
+
+
+def count_disjoint_connection_genes(connections_a, connections_b):
+    """
+    Counts disjoint genes of two lists of connections.
+    :type connections_b: list(ConnectionGene)
+    :type connections_a: list(ConnectionGene)
+    :return: number of disjoint connection genes
+    """
+    innovations_a = [c.innovation_number for c in connections_a]
+    innovations_b = [c.innovation_number for c in connections_b]
+
+    max_innov_a = max(innovations_a)
+    max_innov_b = max(innovations_b)
+
+    # obtain the common max innovation number
+    common_max_innov = min(max_innov_a, max_innov_b)
+
+    # get innovation numbers present only in either a or b's innovation numbers
+    innovations_xor = set(innovations_a) ^ set(innovations_b)
+
+    # count innovation_xor elements which are LE than common max innovation number
+    # (greater innovation numbers are excess connections)
+    return sum(1 for i in innovations_xor if i <= common_max_innov)
+
+
+def count_excess_connection_genes(connections_a, connections_b):
+    """
+    Counts excess genes of two lists of connections.
+    :type connections_a: list(ConnectionGene)
+    :type connections_b: list(ConnectionGene)
+    :return: number of excess connection genes
+    """
+    innovations_a = [c.innovation_number for c in connections_a]
+    innovations_b = [c.innovation_number for c in connections_b]
+
+    max_innov_a = max(innovations_a)
+    max_innov_b = max(innovations_b)
+
+    # obtain the common max innovation number
+    common_max_innov = min(max_innov_a, max_innov_b)
+
+    # get innovation numbers present only in either a or b's innovation numbers
+    innovations_xor = set(innovations_a) ^ set(innovations_b)
+
+    # count innovation_xor elements which are greater than common max innovation number
+    # (LE innovation numbers are disjoint connections)
+    return sum(1 for i in innovations_xor if i > common_max_innov)
+
+
+def count_avg_weight_difference(connections_a, connections_b):
+    """
+    Counts average weight difference between corresponding genes with the same innovation number.
+    :type connections_a: list(ConnectionGene)
+    :type connections_b: list(ConnectionGene)
+    :return: average weight difference
+    """
+    weight_differences = [abs(a.weight - b.weight)
+                          for a in connections_a
+                          for b in connections_b
+                          if a.innovation_number is b.innovation_number]
+    return sum(weight_differences) / float(len(weight_differences))
---
+++ tests/utiltests.py
@@ -0,0 +1,54 @@
+from evolution.util import *
+from evolution.genome import ConnectionGene
+from copy import copy
+import unittest
+
+
+class UtilTestCase(unittest.TestCase):
+    def setUp(self):
+        # stanley example crossover of 2 genomes
+        def getConn(weight):
+            return ConnectionGene(0, 0, weight, True)
+
+        # each connection have innovation number the same as its weight
+        # number of excess: 2 (9 and 10)
+        # number of disjoint: 3 (6, 7, 8)
+        # avg weight difference: 0 (common connections have the same weight from 1 to 5)
+        getConn(1)
+        self.connections_a = []
+        self.connections_b = []
+        self.connections_a = [
+            getConn(1),
+            getConn(2),
+            getConn(3),
+            getConn(4),
+            getConn(5)
+        ]
+        self.connections_b = copy(self.connections_a)
+
+        self.connections_b.append(getConn(6))
+        self.connections_b.append(getConn(7))
+
+        self.connections_a.append(getConn(8))
+
+        self.connections_b.append(getConn(9))
+        self.connections_b.append(getConn(10))
+
+        self.connections_a = sorted(self.connections_a, key=lambda x: x.innovation_number)
+        self.connections_b = sorted(self.connections_b, key=lambda x: x.innovation_number)
+        print([x.innovation_number for x in self.connections_a])
+        print([x.innovation_number for x in self.connections_b])
+
+    def test_count_disjoint_connection_genes(self):
+        self.assertEqual(3, count_disjoint_connection_genes(self.connections_a, self.connections_b))
+
+    def test_count_excess_connection_genes(self):
+        self.assertEqual(2, count_excess_connection_genes(self.connections_a, self.connections_b))
+
+    def test_count_avg_weight_difference(self):
+        self.assertEqual(0, count_avg_weight_difference(self.connections_a, self.connections_b))
+
+
+if __name__ == '__main__':
+    firstSuite = unittest.TestLoader().loadTestsFromTestCase(UtilTestCase)
+    unittest.TextTestRunner(verbosity=2).run(firstSuite)
