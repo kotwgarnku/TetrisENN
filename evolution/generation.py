a2535c4e244affa79c7612eac01d176877a34380
Filip Konieczny <filkonieczny@gmail.com>
2018-01-09 22:33:05 +0100

    Refactoring algorytmu.

--- evolution/generation.py
+++ evolution/generation.py
@@ -26,11 +26,30 @@ class Evaluate(Thread):
 
 
 class Generation:
-    def __init__(self):
+    _specie_number = 0
+
+    def __init__(self, mutation_coefficients=None, compatibility_coefficients=None, compatibility_threshold=6.0):
         self.species = {}
         self.fitness = None
 
-    def create_new_generation(self, mutation_coefficients=None, compatibility_coefficients=None, compatibility_threshold=6.0):
+        if mutation_coefficients is None:
+            self.mutation_coefficients = {
+                'add_connection': 0.1,
+                'split_connection': 0.3,
+                'change_weight': 0.5,
+                'new_connection_abs_max_weight': 1.0,
+                'max_weight_mutation': 0.5
+            }
+        if compatibility_coefficients is None:
+            self.compatibility_coefficients = {
+                'excess_factor': 2.0,
+                'disjoint_factor': 2.0,
+                'weight_difference_factor': 1.0
+            }
+        self.compatibility_threshold = compatibility_threshold
+        self.r_factor = 0.2
+
+    def create_new_generation(self):
         """
         Creates and returns new Generation of species based on current generation.
         """
@@ -60,86 +79,73 @@ class Generation:
         for specie in self.species.values():
             specie.adjust_fitness()
 
-        # calculate mean generation fitness
+        # calculate mean adjusted generation fitness
         self.fitness = sum(phenotypes_fitness) / float(len(phenotypes_fitness)**2)
 
-        # TODO: make 'coefficients' parameters
-        if mutation_coefficients is None:
-            mutation_coefficients = {
-                'add_connection': 0.3,
-                'split_connection': 0.2,
-                'change_weight': 0.5,
-                'new_connection_abs_max_weight': 1.0,
-                'max_weight_mutation': 0.5
-            }
-        if compatibility_coefficients is None:
-            compatibility_coefficients = {
-                'excess_factor': 2.0,
-                'disjoint_factor': 2.0,
-                'weight_difference_factor': 1.0
-            }
+        # create place for new species
+        new_species = {k: Specie() for k in self.species.keys()}
 
-        new_species = []
-        for specie in self.species.values():
+        for (key, specie) in self.species.items():
             specie_offspring_len = round(specie.get_fitness() / self.fitness)
-            # TODO: make 'r' a parameter
-            parents = specie.get_parents(0.2)
-            if len(parents) == 0:
-                continue
-            specie_parents = list(specie.genomes.keys())
+            parents = specie.get_parents(self.r_factor)
 
+            # create specie_offspring_len children
             for i in range(specie_offspring_len):
-                parent1 = random.choice(parents)
-                parent2 = random.choice(parents)
+                # create child
+                offspring = Specie.get_offspring(parents)
+                # mutate it
+                offspring.mutate(self.mutation_coefficients)
 
-                offspring = Genome.reproduce(parent1, parent2)
+                # select specie for the child
 
-                offspring.mutate(mutation_coefficients)
-
-                compatibility_distance = offspring.compatibility_distance(specie.get_representative(), compatibility_coefficients)
-                
-                specie_found = False
                 # favor parents' specie
-                # TODO: add compatibility_threshold
-                if compatibility_distance < compatibility_threshold:
-                    specie.add_genome(offspring)
-                    specie_found = True
+                if self._is_specie_fitting_for_offspring(specie, offspring):
+                    new_species[key].add_genome(offspring)
+                    continue
+                # child does not fit in parents' specie
+                # check if it does in some other specie
                 else:
-                    for sp in self.species.values():
-                        if len(sp.genomes) == 0:
-                            continue
-                        compatibility_distance = offspring.compatibility_distance(sp.get_representative(), compatibility_coefficients)
-                        if compatibility_distance < compatibility_threshold:
-                            sp.add_genome(offspring)
-                            specie_found = True
+                    fitting_specie_found = False
+                    for (key, specie) in self.species.items():
+                        if self._is_specie_fitting_for_offspring(specie, offspring):
+                            new_species[key].add_genome(offspring)
+                            fitting_specie_found = True
                             break
-                if not specie_found:
-                    for sp in new_species:
-                        compatibility_distance = offspring.compatibility_distance(sp.get_representative(),
-                                                                                  compatibility_coefficients)
-                        if compatibility_distance < compatibility_threshold:
-                            sp.add_genome(offspring)
-                            specie_found = True
-                            break
-                    if specie_found:
-                        continue
+                # child does not fit in any of the current species
+                # create new species and add child as it's representative
+                if not fitting_specie_found:
                     # create new specie with offspring as it's representative
                     new_specie = Specie()
                     new_specie.add_genome(offspring)
-                    new_species.append(new_specie)
+                    new_species[Generation._get_new_specie_number()] = new_specie
 
-            # remove parents leaving only children in new specie
-            for i in specie_parents:
-                del specie.genomes[i]
-        for specie in new_species:
-            self.species[len(self.species)] = specie
+        self.species = new_species
+
+    def _is_specie_fitting_for_offspring(self, specie, offspring):
+        is_fitting = False
+        compatibility_distance = offspring.compatibility_distance(specie.get_representative(), self.compatibility_coefficients)
+
+        if compatibility_distance < self.compatibility_threshold:
+            specie.add_genome(offspring)
+            is_fitting = True
+
+        return is_fitting
+
+    @staticmethod
+    def _get_new_specie_number():
+        Generation._specie_number += 1
+        return Generation._specie_number - 1
 
 class Specie:
+    _genome_number = 0
+
     def __init__(self):
         self.genomes = {}
-    
+        self.fitness = None
+
     def add_genome(self, genome):
-        self.genomes[len(self.genomes)] = genome
+        self.genome_number = Specie._get_new_genome_number()
+        self.genomes[self.genome_number] = genome
 
     def adjust_fitness(self):
         """
@@ -148,8 +154,10 @@ class Specie:
         for genome in self.genomes.values():
             genome.fitness /= float(len(self.genomes))
 
+        self.fitness = sum(genome.fitness for genome in self.genomes.values())
+
     def get_fitness(self):
-        return sum(genome.fitness for genome in self.genomes.values())
+        return self.fitness
 
     def get_phenotypes(self):
         return [NeuralNetwork(genome) for genome in self.genomes.values()]
@@ -163,7 +171,22 @@ class Specie:
         """
         # sort genomes by fitness
         genomes = sorted([genome for genome in self.genomes.values()], key=lambda x: x.fitness, reverse=True)
-        # get r% of best-performing genomes
-        genomes = genomes[:round(r*len(genomes))]
+        # get r% of best-performing genomes ( minimum 2 )
+        count_to_return = max(round(r*len(genomes)), 2)
+        genomes = genomes[:count_to_return]
         shuffle(genomes)
         return genomes
+
+    @staticmethod
+    def get_offspring(parents):
+        parent1 = random.choice(parents)
+        parent2 = random.choice(parents)
+
+        offspring = Genome.reproduce(parent1, parent2)
+
+        return offspring
+
+    @staticmethod
+    def _get_new_genome_number():
+        Specie._genome_number += 1
+        return Specie._genome_number - 1
