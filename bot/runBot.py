5bd70c5efed2040e3034700b2733a04f88ea4d22
Kacper Kamiński <lampo100@gmail.com>
2018-02-17 01:13:46 +0100

    seem to work

--- bot/runBot.py
+++ bot/runBot.py
@@ -9,30 +9,30 @@ def create_generation():
     Group._GROUP_ID = 0
     Generation._GENERATION_ID = 0
     specie = Group()
-    for i in range(230):
-        for j in range(231, 235):
+    for i in range(231):
+        for j in range(232, 236):
             ConnectionGene(i + 1, j, enabled=True)
 
     connection_list = []
     z = 0
-    for i in range(230):
-        for j in range(231, 235):
+    for i in range(231):
+        for j in range(232, 236):
             connection_list.append([i + 1, j, random.normalvariate(mu=0.0, sigma=1.0), True, z])
             z += 1
 
-    for i in range(10):
-        specie.add_genome(Genome(connection_list, 230, 4))
+    for i in range(20):
+        specie.add_genome(Genome(connection_list, 231, 4))
 
     mutation_coefficients = {
         'add_connection': 0.5,
         'split_connection': 0.2,
         'change_weight': 0.8,
-        'new_connection_abs_max_weight': 1.0,
+        'new_connection_abs_max_weight': 2.0,
         'max_weight_mutation': 0.5
     }
     compatibility_coefficients = {
-        'excess_factor': 1.0,
-        'disjoint_factor': 1.0,
+        'excess_factor': 1.5,
+        'disjoint_factor': 1.5,
         'weight_difference_factor': 2.0
     }
     log = Logger()
